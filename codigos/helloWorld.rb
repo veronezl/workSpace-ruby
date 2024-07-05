9fcf40b6bb70474a212c05df259ae21abe66c7b1
lucasveronez <lucasveronez.dev@gmail.com>
2024-07-04 21:24:30 -0300

    segundo commit

--- codigos/helloWorld.rb
+++ codigos/helloWorld.rb
@@ -1,1 +1,11 @@
 puts "Hello World"
+# isso é uma comentário
+array = ['position_0', 'position_1', 'position_2']
+puts array[0]
+
+int = 10
+float = 10.5
+
+hash = {course: 'ruby', language: 'pt-br', locale: 'dio.com.br'}
+hash[:course]
+
