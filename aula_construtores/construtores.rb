9b2572ca80a158af67f6afb0fe20e5502e477f8b
veronezl <lucasveronez.dev@gmail.com>
2024-07-11 10:32:14 -0300

    feat: add poo, parte 2, atributos, variavies e construtores

---
+++ aula_construtores/construtores.rb
@@ -0,0 +1,16 @@
+class Person
+  def initialize(name,age)
+    @name = name
+    @age = age
+  end
+
+  def check
+    puts "Instãncia de classe iniciada com os valores:"
+    puts "Nome: #{@name}"
+    puts "Idade: #{@age}"
+  end
+end
+
+  person = Person.new('João', 12)
+  person.check
+
